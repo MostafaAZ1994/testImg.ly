31a849624dbceb2f7b2996f9659bbaefff72cf94
MostafaAZ1994 <mostafa@customsurgical.co>
2023-01-18 12:07:46 +0400

    Initial Commit

---
+++ testImgly/testImglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  testImglyApp.swift
+//  testImgly
+//
+//  Created by Mostafa ALZOUBI on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct testImglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

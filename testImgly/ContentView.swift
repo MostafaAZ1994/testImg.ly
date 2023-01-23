2c88e069d32fd2a90be76eb879935080dc5ec4a4
MostafaAZ1994 <mostafa@customsurgical.co>
2023-01-23 17:59:59 +0400

    test imply

--- testImgly/ContentView.swift
+++ testImgly/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+
+class Example: NSObject {
+    /// The view controller that this example is being invoked from.
+    weak var presentingViewController: UIViewController?
+
+    func invokeExample() {
+    }
+}
 struct ContentView: View {
+    @State var presentVideoEditor = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+            Button {
+              presentVideoEditor = true
+            } label: {
+                Text("try imgley")
+            }
+
         }
         .padding()
+        .sheet(isPresented: $presentVideoEditor) {
+            VideoEditorSwiftUIView(video: Video(url: Bundle.main.url(forResource: "30 Minute Timer", withExtension: "mp4")!))
+        }
+       
     }
 }
 
@@ -24,3 +40,62 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+import SwiftUI
+import UIKit
+import VideoEditorSDK
+import ImglyKit
+import AVFoundation
+import Foundation
+import UniformTypeIdentifiers
+
+class ShowVideoEditorSwiftUISwift: Example, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+  override func invokeExample() {
+
+      let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.mediaTypes = [UTType.movie.identifier]
+
+        // Setting `videoExportPreset` to passthrough will not reencode the video before passing it to
+        // the delegate methods.
+        imagePicker.videoExportPreset = AVAssetExportPresetPassthrough
+      imagePicker.modalPresentationStyle = .fullScreen
+        // Present the image picker modally.
+    presentingViewController?.present(imagePicker, animated: true, completion: nil)
+  }
+}
+
+// A `View` that hosts the `VideoEditor` in order
+// to use it in this `UIKit` example application.
+struct VideoEditorSwiftUIView: View {
+  // The action to dismiss the view.
+  internal var dismissAction: (() -> Void)?
+
+  // The video being edited.
+  let video: Video
+
+  var body: some View {
+    VideoEditor(video: video)
+      .onDidSave { result in
+        // The user exported a new video successfully and the newly generated video is located at `result.output.url` of the returned `VideoEditorResult`. Dismissing the editor.
+        print("Received video at \(result.output.url.absoluteString)")
+        dismissAction?()
+      }
+      .onDidCancel {
+        // The user tapped on the cancel button within the editor. Dismissing the editor.
+        dismissAction?()
+      }
+      .onDidFail { error in
+        // There was an error generating the video.
+        print("Editor finished with error: \(error.localizedDescription)")
+        // Dismissing the editor.
+        dismissAction?()
+      }
+      // In order for the editor to fill out the whole screen it needs
+      // to ignore the safe area.
+      .ignoresSafeArea()
+  }
+}
+
